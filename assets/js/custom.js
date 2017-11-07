d5414d1f4c22553fb7d11b6e4143e647561a7b97
Sergio Pereira Mega <proagua2@gmail.com>
2017-11-06 21:47:27 -0300

    foto slider botones alternoscabeza cortada

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -112,6 +112,7 @@
         ------------------------------------------------------------- */
         if ($('.home-slider').length){
             $('.home-slider').owlCarousel({
+                loop:true,
                 nav:true,
                 items: 1
             });
@@ -346,7 +347,7 @@
             // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
             $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
         })
-        
+
         // stop playing the youtube video when I close the modal
         $('#myModal').on('hide.bs.modal', function (e) {
             // a poor man's stop video
